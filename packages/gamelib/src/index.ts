18d734e38d58b7eda3f9c635b8f0c8b466df6837
Remi Rampin <remi@rampin.org>
2022-05-04 22:04:04 -0400

    Simplest game protocol

--- packages/gamelib/src/index.ts
+++ packages/gamelib/src/index.ts
@@ -1,1 +1,11 @@
-export const APP_TITLE = 'Play Superjack';
+export class Game {
+  num: number;
+
+  constructor() {
+    this.num = 0;
+  }
+
+  increment(by: number): void {
+    this.num += by;
+  }
+}
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,4 +1,4 @@
-import { APP_TITLE } from 'superjack';
+import { Game } from 'superjack';
 import cors from 'cors';
 import express from 'express';
 import { join } from 'path';
@@ -7,13 +7,24 @@ const PORT = 3000;
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 // Serve static resources from the "public" folder (ex: when there are images to display)
 app.use(express.static(join(__dirname, '../../frontend/public')));
 
 // Serve the HTML page
-app.get('*', (req: any, res: any) => {
+app.get('/', (req: any, res: any) => {
   res.sendFile(join(__dirname, '../../frontend/public', 'index.html'));
+});
+
+// API
+const game = new Game();
+app.get('/api', (req: any, res: any) => {
+  res.status(200).json({'num': game.num});
+});
+app.post('/api', (req: any, res:any) => {
+  game.increment(req.body.num);
+  res.status(200).json({'num': game.num});
 });
 
 app.listen(PORT, () => {
